c4373b0721d3e75eec451990d33ac8415c770a9b
Michael King <mdk.email@gmail.com>
2026-03-19 23:43:33 -0500

    Fix publish build version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.gpgamelab.justpatience"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
