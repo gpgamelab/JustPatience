3c018b99770c79b934c3af35288d1999e9426279
Michael King <mdk.email@gmail.com>
2026-04-06 23:18:34 -0500

    Release 1.0.4 to the play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.gpgamelab.justpatience"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
