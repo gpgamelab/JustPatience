013cf3d19844438c322e60b42a2870de54fb573e
Michael King <mdk.email@gmail.com>
2026-08-12 19:55:15 -0500

    Release 1.1.6 build 106

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.gpgamelab.justpatience"
         minSdk = 30
         targetSdk = 36
-        versionCode = 105
-        versionName = "1.1.5"
+        versionCode = 106
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
